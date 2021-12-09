e0231a80e114d32b220e0c5ac89b6d3a02b6afbb
AchRef <78941591+AchRef864@users.noreply.github.com>
2021-12-09 22:41:39 +0100

    Update Facebook-Login.py

--- Facebook-Login.py
+++ Facebook-Login.py
@@ -6,7 +6,6 @@ PATH = ("D:\Education\edgedriver_win64\msedgedriver.exe")
 driver = webdriver.Edge(PATH)
 
 driver.get("https://www.facebook.com/")
-# FullXPath = /html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input
 
 search1 = driver.find_element_by_name("email")
 search1.send_keys("your email")
