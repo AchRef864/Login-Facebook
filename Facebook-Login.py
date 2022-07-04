31069bb96331cc61185df2ade9ed8c17d5d7089b
AchRef <78941591+AchRef864@users.noreply.github.com>
2022-07-04 16:11:44 +0100

    Update Facebook-Login.py

--- Facebook-Login.py
+++ Facebook-Login.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+Email = input("write you email or phone number")
+Password = input("enter your password")
+
 PATH = ("{the path of the msedgedriver}\msedgedriver.exe") 
 driver = webdriver.Edge(PATH)
 
 driver.get("https://www.facebook.com/")
 
 search1 = driver.find_element_by_name("email")
-search1.send_keys("your email")
+search1.send_keys(Email)
 search2 = driver.find_element_by_name("pass")
-search2.send_keys("your password")
+search2.send_keys(Password)
 search1.send_keys(Keys.RETURN)
